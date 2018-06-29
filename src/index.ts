7dc74126253fe943c2cfcf4418ccf151d0e96a6e
James Monger <jkmonger96@gmail.com>
2018-06-29 01:58:49 +0100

    Create SetInterfaceHiddenUntilHovered

--- src/index.ts
+++ src/index.ts
@@ -37,3 +37,4 @@ export { SetFriendsListStatusMessage } from "./messages/set-friends-list-status"
 export { HideAllInterfacesMessage } from "./messages/hide-all-interfaces";
 export { CreateProjectileMessage } from "./messages/create-projectile";
 export { CreateObjectMessage } from "./messages/create-object";
+export { SetInterfaceHiddenUntilHovered } from "./messages/set-interface-hidden-until-hovered";
---
+++ src/messages/set-interface-hidden-until-hovered.ts
@@ -0,0 +1,4 @@
+export interface SetInterfaceHiddenUntilHovered {
+    hidden: boolean;
+    interfaceId: number;
+}
